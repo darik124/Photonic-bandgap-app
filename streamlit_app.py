8a6c66bb4692eb2f78d967556a3d5be705683d94
darik124 <161494163+darik124@users.noreply.github.com>
2025-08-08 16:19:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,3 @@
-# Photonic Band Gap Visualizer
-
-An interactive Streamlit web app for simulating and visualizing the behavior of 2D dielectric photonic band gap (PBG) materials.
-This tool allows users to adjust structural parameters—such as rod diameter, spacing, and permittivity—and view how those affect electromagnetic wave transmission
-
-## Features
-- Adjustable permittivity, diameter, spacing, and rod count
-- Simulated transmission spectrum
-- Easy deployment via Streamlit Cloud
-
-## Author
-Darikson Valenzuela
-
-streamlit
-matplotlib
-numpy
-
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
